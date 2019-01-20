0d3b6cf40b7e052c85355214b743af5abd20646c
SEKINAT ALIU <sekinataliu1@Sekinats-Air.T-mobile.com>
2019-01-20 11:09:05 -0800

    Feat:add CRUD functionality

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,49 +1,92 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
-# Simulates database note in database would not need to asgin college_id
-williams = {'image_filename': 'williams_college.jpg', 'location': '880 Main St. Williamstown, MA 01267', 'phone_number':'(413) 597-3131','college_type':'Liberal Arts College','notes':'Your thoughts here','college_id':'1','region':'Northeast','name':'Williams College'}
+from database_setup import College, Region, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-colleges = [{'image_filename': 'williams_college.jpg', 'location': '880 Main St. Williamstown, MA 01267', 'phone_number':'(413) 597-3131','college_type':'Liberal Arts College','notes':'Your thoughts here','college_id':'1','region':'Northeast','name':'Williams College'},
-            {'image_filename':'usc_college.jpg', 'location':'Los Angeles, CA 90007','phone_number':'(213) 740-2311','college_type':'Private University','notes':'Your thoughts here','college_id':'2','region':'West','name':'University of Southern California'},
-            {'image_filename':'uni_iowa.jpg','location':'Iowa City, IA 52242','phone_number':'(319) 335-3500','college_type':'Public Research University','notes':'Your thoughts here','college_id':'3','region':'Midwest','name':'University of Iowa'},
-            {'image_filename':'uni_montana.jpg','location':' 32 Campus Dr, Missoula, MT 59812','phone_number':'(406) 243-0211','college_type':'Public Research University','notes':'Your thoughts here','college_id':'4','region':'West','name':'University of Montana'}]
-
-regions = ['West', 'Midwest', 'Northeast']
-
+# Create session and connect to DB ##
+engine = create_engine('sqlite:///colleges.db?check_same_thread=False')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #show all regions
 @app.route('/')
 @app.route('/region/')
 def showRegions():
-    return render_template('availableregions.html', colleges=colleges, regions=regions)
+    regions = session.query(Region).all()
+    return render_template('availableregions.html', regions=regions)
 
 #show all colleges for region
 @app.route('/region/<region>/')
 def showRegionColleges(region):
-    return render_template('regionalcolleges.html', colleges=colleges, region=region)
+    # get id of region (not provided in url for simple url)
+    region_info = session.query(Region).filter_by(name=region).one()
+    # filter id
+    regioncolleges = session.query(College).filter_by(college_region_id=region_info.id)
+    return render_template('regionalcolleges.html', colleges=regioncolleges, region=region)
 
 # create new college in same region by clicking link on page that shows all colleges for region
-@app.route('/region/<region>/new/')
+@app.route('/region/<region>/new/', methods=['GET','POST'])
 def addNewCollege(region):
-    # make sure that when adding item that region incorperated
-    return render_template('addregionalcollege.html', region=region, colleges=colleges)
+    if request.method == 'POST':
+        newCollege = College(name = request.form['college_name'], college_region=region, location = request.form['location'], phone_number = request.form['phone_number'], college_type = request.form['college_type'], notes = request.form['client_notes'])
+        session.add(newCollege)
+        session.commit()
+        # return render_template('regionalcolleges.html', colleges=colleges, region=region)
+        redirect(url_for('showRegionColleges',region=region))
+    else:
+        return render_template('addregionalcollege.html', region=region)
 
 # show college by clicking link on page that shows all colleges for region
 @app.route('/region/<region>/<int:college_id>/')
 def showMyCollege(region, college_id):
-    # will filter through for cillege that matches id
-    return render_template('mycollege.html', college=williams, colleges=colleges, region=region)
+    # get region colleges to provide for render template
+    region_info = session.query(Region).filter_by(name=region).one()
+    regioncolleges = session.query(College).filter_by(college_region_id=region_info.id)
+
+    # get college to view
+    college = session.query(College).filter_by(college_id=college_id).one()
+    return render_template('mycollege.html', college=college, colleges=regioncolleges, region=region, image=college.image_filename)
 
 # may go to another page to edit any of college info provided
-@app.route('/region/<region>/<int:college_id>/edit', methods=['GET','POST'])
+@app.route('/region/<region>/<int:college_id>/edit/', methods=['GET','POST'])
 def editMyCollege(region, college_id):
-    return render_template('editregionalcollege.html',region=region,college_id=college_id, item=williams)
+    editedItem = session.query(College).filter_by(college_id=college_id).one()
+    if request.method == 'POST':
+        # change college_name  phone_number college_type client_notes
+        if request.form['college_name']:
+            editedItem.name = request.form['college_name']
+        # change location
+        if request.form['location']:
+            editedItem.location = request.form['location']
+        # change phone_number
+        if request.form['phone_number']:
+            editedItem.phone_number = request.form['phone_number']
+        # change college_type
+        if request.form['college_type']:
+            editedItem.college_type = request.form['college_type']
+        # change client_notes
+        if request.form['client_notes']:
+            editedItem.notes = request.form['client_notes']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('showMyCollege', region=region, college_id=college_id))
+    else:
+        return render_template('editregionalcollege.html',region=region, college_id=college_id, item=editedItem)
 
 # this page allows user to delete colleges
 @app.route('/region/<region>/<int:college_id>/delete/')
 def deleteMyCollege(region, college_id):
-    return render_template('deletemycollege.html',region=region,college_id=college_id, item=williams)
+    itemToDelete = session.query(College).filter_by(college_id=college_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('showMyCollege', region=region, college_id=college_id))
+    else:
+        # if get request
+        return render_template('deletemycollege.html',region=region,college_id=college_id, item=itemToDelete)
 
 
 if __name__ == '__main__':
